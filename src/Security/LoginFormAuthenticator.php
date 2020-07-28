221de199568d1a7dc69bb87c5ad6ca34130a28b4
JesusCH92 <jcocaharo@hotmail.com>
2020-07-28 15:27:51 +0000

    comment csrf token in registration form

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -50,7 +50,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         $credentials = [
             'email' => $request->request->get('email'),
             'password' => $request->request->get('password'),
-            'csrf_token' => $request->request->get('_csrf_token'),
+            // 'csrf_token' => $request->request->get('_csrf_token'),
         ];
         $request->getSession()->set(
             Security::LAST_USERNAME,
