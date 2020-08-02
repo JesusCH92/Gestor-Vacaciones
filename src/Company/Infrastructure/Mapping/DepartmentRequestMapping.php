851a9eda4495fd2e3294f47e38d8c7a35e3d0462
JesusCH92 <jcocaharo@hotmail.com>
2020-08-02 22:48:35 +0200

    create class from mapping to DepartmentRequest to Department Entity

---
+++ src/Company/Infrastructure/Mapping/DepartmentRequestMapping.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Company\Infrastructure\Mapping;
+
+use App\Entity\Department;
+use App\Entity\Company;
+use App\Company\ApplicationService\DepartmentRequest;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class DepartmentRequestMapping
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function __invoke(DepartmentRequest $departmentRequest)
+    {
+        $departmentEnity = new Department();
+        $companyRepository = $this->entityManager->getRepository(Company:: class);
+
+        $companyById = $companyRepository->findBy(
+            [ 'id' => $departmentRequest->companyId() ]
+        );
+
+        $departmentEnity->setCompany($companyById[0]);
+        $departmentEnity->setDepartmentname($departmentRequest->departmentName());
+        $departmentEnity->setDepartmentcode($departmentRequest->departmentCode());
+
+        return $departmentEnity;
+    }
+}
