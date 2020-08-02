11af167063cc03eda10d7f23ef168efad530a663
JesusCH92 <jcocaharo@hotmail.com>
2020-08-02 20:36:08 +0200

    add AJAX method (post) for create a department

--- public/assets/js/CompanyModule/CompanyModule.js
+++ public/assets/js/CompanyModule/CompanyModule.js
@@ -1,4 +1,40 @@
 var companyModule = (function(){
-    console.log('Hello there from Company Module');
+    var $addDepartmentBtn = $("#dpt-add-btn");
+    var $departmentNameAdded = $("#department-name-added");
+    var $codeDepartmentAdded = $("#code-department-added");
+
+    var createDepartment = function({department, callback = console.log}) {
+        $.ajax({
+            type: 'POST',
+            url: '/company/management/add/department',
+            async: true,
+            data: {department},
+            success: function(data){
+                // callback({departmentAdded: data.department_format});
+                callback(data);
+            }
+        });
+    };
+
+    var initEvents =  function(){
+
+        $addDepartmentBtn.click(function(){
+            console.log('General Kenobi');
+            var $departmentName = $departmentNameAdded.val().trim();
+            var $departmentCode = $codeDepartmentAdded.val().trim();
+            console.log($departmentName);
+            console.log($departmentCode);
+
+            createDepartment({
+                department : {
+                    name : $departmentName,
+                    code : $departmentCode
+                }
+            })
+
+        });
+    }
+
+    initEvents();
     
 })();
