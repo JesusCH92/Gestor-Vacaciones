d01b1ecf196373909e6176a24e28b8af127597b4
JesusCH92 <jcocaharo@hotmail.com>
2020-08-03 23:27:33 +0200

    create model for make AJAX request to server

---
+++ public/assets/js/CompanyModule/CompanyModel.js
@@ -0,0 +1,25 @@
+var CompanyModelModule = (function(){
+    // var $departmentContainer = $("#departments-container");
+
+    var createDepartment = function({department, callback = console.log, container}) {
+        $.ajax({
+            type: 'POST',
+            url: '/company/management/add/department',
+            async: true,
+            data: {department},
+            success: function(data){
+                callback({
+                    containerDepartment : container,
+                    department : data.department_created
+                });
+            },
+            error: function(data){
+                console.log(data);
+            }
+        });
+    };
+
+    return{
+        createDepartment : createDepartment
+    }
+})();
