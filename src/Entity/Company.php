34fd06a1996f7dd43b594ca49a107611e7fbdd4c
JesusCH92 <jcocaharo@hotmail.com>
2020-08-06 16:42:42 +0200

    delete default Repository for Company entity

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\CompanyRepository")
+ * @ORM\Entity
  * @ORM\Table(name="company", options={"collate"="utf8_general_ci", "charset"="utf8"})
  */
 class Company
