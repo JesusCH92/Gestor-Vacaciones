ed2c250c19a79f4caa3c52c146a3d1af316ecbe9
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-08-08 17:46:08 +0200

    create entity DayOffFormRequest for save in bd each day selected in a day off form

---
+++ src/Entity/DayOffFormRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DayOffFormRequestRepository")
+ * @ORM\Table(name="day_off_form_request", options={"collate"="utf8_general_ci", "charset"="utf8"})
+ */
+class DayOffFormRequest
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=DayOffForm::class)
+     * @ORM\JoinColumn(name="code_day_off_form", referencedColumnName="code_day_off_form", nullable=false)
+     */
+    private $codeDayOffForm;
+
+
+    /** @ORM\Embedded(class = "App\DayOff\Domain\ValueObject\DayOffSelected", columnPrefix = false) */
+    private $dayOffSelected;
+
+    /**
+     * DayOffFormRequest constructor.
+     * @param $codeDayOffForm
+     * @param $dayOffSelected
+     */
+    public function __construct($codeDayOffForm, $dayOffSelected)
+    {
+        $this->codeDayOffForm = $codeDayOffForm;
+        $this->dayOffSelected = $dayOffSelected;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDayOffSelected(): ?DateTimeInterface
+    {
+        return $this->dayOffSelected;
+    }
+
+    public function setDayOffSelected(DateTimeInterface $dayOffSelected): self
+    {
+        $this->dayOffSelected = $dayOffSelected;
+
+        return $this;
+    }
+
+    public function getCodeDayOffForm()
+    {
+        return $this->codeDayOffForm;
+    }
+
+}
