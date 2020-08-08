d9d197d344a3354ca8ca05cf98a9a5e01fae6aa2
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-08-08 17:53:18 +0200

    create implementation class of DayOffRepository for mysql db

---
+++ src/DayOff/Infrastructure/DoctrineDayOffRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\DayOff\Infrastructure;
+
+
+use App\DayOff\Domain\DayOffRepository;
+use App\Entity\DayOffForm;
+use App\Entity\DayOffFormRequest;
+use App\User\Domain\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class DoctrineDayOffRepository implements DayOffRepository
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    public function findOne(User $user)
+    {
+        $dayOffRepository = $this->entityManager->getRepository(DayOffForm::class);
+        return $dayOffRepository->findBy(['idUser' => $user]);
+    }
+
+    public function saveDayOffForm(DayOffForm $dayOffForm)
+    {
+        $this->entityManager->persist($dayOffForm);
+        $this->entityManager->flush();
+    }
+
+    public function saveDayOffFormRequest(DayOffFormRequest $dayOffFormRequest)
+    {
+        $this->entityManager->persist($dayOffFormRequest);
+        $this->entityManager->flush();
+    }
+}
