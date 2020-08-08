8f73a881d8aa8145e0cfdebb9d7fe9ab2376adc1
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-08-08 17:39:30 +0200

    create Value Object embeddable for DayOffForm entity with the function to change the status to accepted if its supervisor

---
+++ src/DayOff/Domain/ValueObject/StatusDayOffForm.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\DayOff\Domain\ValueObject;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Embeddable */
+final class StatusDayOffForm
+{
+    const STATUS = [
+        'ROLE_USER' => 'PENDING',
+        'ROLE_SUPERVISOR' => 'ACCEPTED'
+    ];
+    /**
+     * @ORM\Column(type="string",name="status_day_off_form", length=15)
+     */
+    private $statusDayOffForm;
+
+    /**
+     * StatusDayOffForm constructor.
+     * @param $statusDayOffForm
+     */
+    public function __construct($statusDayOffForm)
+    {
+        $this->statusDayOffForm = $statusDayOffForm;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatusDayOffForm()
+    {
+        return $this->statusDayOffForm;
+    }
+
+    public function statusByUserRole(string $role)
+    {
+        $this->statusDayOffForm= self::STATUS[$role];
+    }
+}
