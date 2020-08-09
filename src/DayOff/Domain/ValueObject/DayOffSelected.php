666c7897cf7d3d407e822081c61509c90dccd7f0
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-08-09 18:20:37 +0200

    refactor vo: remove 'get' form the getters. Add functions to check if the date is correct

--- src/DayOff/Domain/ValueObject/DayOffSelected.php
+++ src/DayOff/Domain/ValueObject/DayOffSelected.php
@@ -20,18 +20,47 @@ final class DayOffSelected
      * @param $dayOffSelected
      * @throws Exception
      */
-    public function __construct($dayOffSelected)
+    public function __construct(string $dayOffSelected)
     {
+        $this->checkCorrectDateType($dayOffSelected);
         $this->dayOffSelected = new DateTimeImmutable($dayOffSelected);
+    }
+
+    public function checkCorrectDateType(string $dayOffSelected)
+    {
+        $date = explode('-', $dayOffSelected);
+        $this->checkCorrectYearType($date[0]);
+        $this->checkCorrectMonthType($date[1]);
+        $this->checkCorrectDayType($date[2]);
+    }
+
+    public function checkCorrectYearType(string $year): void
+    {
+        if (!4 === strlen($year) || date("Y") > date($year)) {
+            //exception
+        }
+    }
+
+    public function checkCorrectMonthType(string $month): void
+    {
+        if (!2 === strlen($month) || 12 < (int)$month) {
+            //exception
+        }
+    }
+
+    public function checkCorrectDayType(string $day): void
+    {
+        if (!2 === strlen($day) || 31 < (int)$day) {
+            //exception
+        }
     }
 
     /**
      * @return mixed
      */
-    public function getDayOffSelected()
+    public function dayOffSelected()
     {
         return $this->dayOffSelected;
     }
-
 
 }
