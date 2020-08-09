c42b6a53e8b0c87e742cd47e38a21adc4767d41a
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-08-09 18:21:27 +0200

    refactor vo: remove 'get' form the getters. Add functions to change the status of the variable (and return a new vo -> self)

--- src/DayOff/Domain/ValueObject/StatusDayOffForm.php
+++ src/DayOff/Domain/ValueObject/StatusDayOffForm.php
@@ -8,9 +8,24 @@ use Doctrine\ORM\Mapping as ORM;
 /** @ORM\Embeddable */
 final class StatusDayOffForm
 {
+
     const STATUS = [
-        'ROLE_USER' => 'PENDING',
-        'ROLE_SUPERVISOR' => 'ACCEPTED'
+        'HOLIDAY' => [
+            'ROLE_USER' => 'PENDING',
+            'ROLE_SUPERVISOR' => 'ACCEPTED'
+        ],
+        'PERSONAL' => [
+            'ROLE_USER' => 'PENDING',
+            'ROLE_SUPERVISOR' => 'ACCEPTED'
+        ],
+        'OTHER' => [
+            'ROLE_USER' => 'PENDING',
+            'ROLE_SUPERVISOR' => 'ACCEPTED'
+        ],
+        'SICK LEAVE' => [
+            'ROLE_USER' => 'ACCEPTED',
+            'ROLE_SUPERVISOR' => 'ACCEPTED'
+        ]
     ];
     /**
      * @ORM\Column(type="string",name="status_day_off_form", length=15)
@@ -18,21 +33,31 @@ final class StatusDayOffForm
     private $statusDayOffForm;
 
 
-    public function __construct()
+    public function __construct(string $statusDayOffForm = 'PENDING')
     {
-
+        $this->statusDayOffForm = $statusDayOffForm;
     }
 
     /**
      * @return mixed
      */
-    public function getStatusDayOffForm()
+    public function statusDayOffForm()
     {
         return $this->statusDayOffForm;
     }
 
-    public function statusByUserRole(string $role)
+    public function statusByUserRole(string $type, string $role): void
     {
-        $this->statusDayOffForm= self::STATUS[$role];
+        $this->statusDayOffForm = self::STATUS[$type][$role];
+    }
+
+    public function acceptDayOff(): self
+    {
+        return new self('ACCEPTED');
+    }
+
+    public function denyDayOff(): self
+    {
+        return new self('DENIED');
     }
 }
