f91a36300b67a7738a3825e59ae8813f17db51e6
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-08-09 22:35:57 +0200

    add exception InvalidDateTypeException for dates than do not mach the type and dates that not exists. Add  exception InvalidDayOffSelectedException for dates that aren't between the init date and the end date of the calendar

--- src/DayOff/Domain/ValueObject/DayOffSelected.php
+++ src/DayOff/Domain/ValueObject/DayOffSelected.php
@@ -3,6 +3,8 @@
 
 namespace App\DayOff\Domain\ValueObject;
 
+use App\DayOff\Domain\Exception\InvalidDateTypeException;
+use App\DayOff\Domain\Exception\InvalidDayOffSelectedException;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Exception;
@@ -26,33 +28,16 @@ final class DayOffSelected
         $this->dayOffSelected = new DateTimeImmutable($dayOffSelected);
     }
 
-    public function checkCorrectDateType(string $dayOffSelected)
+    private function checkCorrectDateType(string $dayOffSelected)
     {
         $date = explode('-', $dayOffSelected);
-        $this->checkCorrectYearType($date[0]);
+        if (!checkdate($date[1], $date[2], $date[0])) {
+            throw new InvalidDateTypeException();
+        }
+        /*$this->checkCorrectYearType($date[0]);
         $this->checkCorrectMonthType($date[1]);
         $this->checkCorrectDayType($date[2]);
-    }
-
-    public function checkCorrectYearType(string $year): void
-    {
-        if (!4 === strlen($year) || date("Y") > date($year)) {
-            //exception
-        }
-    }
-
-    public function checkCorrectMonthType(string $month): void
-    {
-        if (!2 === strlen($month) || 12 < (int)$month) {
-            //exception
-        }
-    }
-
-    public function checkCorrectDayType(string $day): void
-    {
-        if (!2 === strlen($day) || 31 < (int)$day) {
-            //exception
-        }
+        */
     }
 
     /**
@@ -63,9 +48,32 @@ final class DayOffSelected
         return $this->dayOffSelected;
     }
 
-    public function isCorrectDaySelectedTiming($initDate,$endDate): bool
+    public function isCorrectDaySelectedTiming($initDate, $endDate)
     {
-        return $initDate < $this->dayOffSelected && $endDate > $this->dayOffSelected;
+        if ($initDate > $this->dayOffSelected || $endDate < $this->dayOffSelected) {
+            throw new InvalidDayOffSelectedException($initDate, $endDate);
+        }
+    }
+
+    private function checkCorrectYearType(string $year): void
+    {
+        if (!4 === strlen($year) || date("Y") > date($year)) {
+            throw new InvalidDateTypeException();
+        }
+    }
+
+    private function checkCorrectMonthType(string $month): void
+    {
+        if (!2 === strlen($month) || 12 < (int)$month) {
+            throw new InvalidDateTypeException();
+        }
+    }
+
+    private function checkCorrectDayType(string $day): void
+    {
+        if (!2 === strlen($day) || 31 < (int)$day) {
+            throw new InvalidDateTypeException();
+        }
     }
 
 }
