cec4554c483b0a2072daf47622375e157af0345e
JesusCH92 <jcocaharo@hotmail.com>
2020-08-14 12:25:11 +0200

    add exception when there is calendar with the same working year

---
+++ src/Calendar/ApplicationService/Exception/CalendarAlreadyExistsException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Calendar\ApplicationService\Exception;
+
+use Exception;
+use Throwable;
+
+final class CalendarAlreadyExistsException extends Exception
+{
+    public function __construct(string $workingYear, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct(sprintf('there is already a calendar for the "%s" working year', $workingYear), $code, $previous);
+    }
+}
