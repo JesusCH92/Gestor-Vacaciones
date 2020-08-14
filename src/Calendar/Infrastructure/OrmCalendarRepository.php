74e4bb8bdeeb80601f82894d1e2879dff89f36ac
JesusCH92 <jcocaharo@hotmail.com>
2020-08-14 19:09:24 +0200

    refactor findCalendarByWorkingYear,it's not finished need refactor for save calendar config

--- src/Calendar/Infrastructure/OrmCalendarRepository.php
+++ src/Calendar/Infrastructure/OrmCalendarRepository.php
@@ -6,6 +6,7 @@ namespace App\Calendar\Infrastructure;
 
 use App\Calendar\ApplicationService\DTO\CalendarRequest;
 use App\Calendar\Domain\CalendarRepository;
+use App\Calendar\Domain\ValueObject\WorkDays;
 use App\Calendar\Domain\ValueObject\WorkingYear;
 use App\Entity\Calendar;
 use DateTimeImmutable;
@@ -20,12 +21,13 @@ final class OrmCalendarRepository implements CalendarRepository
         $this->entityManager = $entityManager;
     }
 
-    public function findCalendarByWorkingYear(WorkingYear $workingYear): ?Calendar
+    public function findCalendarByWorkingYear(CalendarRequest $calendarRequest): ?Calendar
     {
+        $workinYear = intval($calendarRequest->workingYear());
         $calendarRepository = $this->entityManager->getRepository(Calendar::class);
         $calendar = $calendarRepository->findBy(
             [
-                'workingYear.workingYear' => $workingYear->workingYear()
+                'workingYear.workingYear' => $workinYear
             ]
         );
 
@@ -37,10 +39,11 @@ final class OrmCalendarRepository implements CalendarRepository
     public function saveCalendar(CalendarRequest $calendarRequest): void
     {
         $workingYear = new WorkingYear(intval($calendarRequest->workingYear()));
+        $workDays = new WorkDays($calendarRequest->workDays());
         $calendarEntity = new Calendar(
             new DateTimeImmutable($calendarRequest->initDateRequest()),
             new DateTimeImmutable($calendarRequest->endDateRequest()),
-            $calendarRequest->workDays(),
+            $workDays,
             $calendarRequest->workDays(),
             $calendarRequest->company(),
             $workingYear
