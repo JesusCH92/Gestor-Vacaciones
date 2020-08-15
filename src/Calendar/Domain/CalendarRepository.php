41aabdfd7cc230f32c3c44e9bd5a84b681a7985d
JesusCH92 <jcocaharo@hotmail.com>
2020-08-15 20:12:45 +0200

    update CalendarRepository for save feastday collection

--- src/Calendar/Domain/CalendarRepository.php
+++ src/Calendar/Domain/CalendarRepository.php
@@ -5,11 +5,10 @@ declare(strict_types = 1);
 namespace App\Calendar\Domain;
 
 use App\Calendar\ApplicationService\DTO\CalendarRequest;
-use App\Calendar\Domain\ValueObject\WorkingYear;
 use App\Entity\Calendar;
 
 interface CalendarRepository
 {
     public function findCalendarByWorkingYear(CalendarRequest $calendarRequest): ?Calendar;
-    public function saveCalendarConfig(Calendar $calendar, array $typeDayOffCollection): void;
+    public function saveCalendarConfig(Calendar $calendar, array $typeDayOffCollection, array $feastdayCollection): void;
 }
