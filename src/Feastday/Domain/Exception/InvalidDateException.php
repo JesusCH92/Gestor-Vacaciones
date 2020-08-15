72627f90bb457d2f0ab30d1ac60654fae9548cfd
JesusCH92 <jcocaharo@hotmail.com>
2020-08-15 20:19:33 +0200

    add exception for check if date is valid (correct date and greater or equal to 2019)

---
+++ src/Feastday/Domain/Exception/InvalidDateException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Featsday\Domain\Exception;
+
+use Exception;
+use Throwable;
+
+final class InvalidDateException extends Exception
+{
+    public function __construct(string $value, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct(sprintf('Provided date "%s" is invalid', $value), $code, $previous);
+    }
+}
