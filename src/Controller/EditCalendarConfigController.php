db8e559aede48fc891b7bd3c10e50165285e55fc
JesusCH92 <jcocaharo@hotmail.com>
2020-08-17 21:31:46 +0200

    create controller for show Edit Calendar view

---
+++ src/Controller/EditCalendarConfigController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use App\Calendar\ApplicationService\GetAllCalendar;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class EditCalendarConfigController extends AbstractController
+{
+    private GetAllCalendar $getAllCalendar;
+
+    public function __construct(GetAllCalendar $getAllCalendar)
+    {
+        $this->getAllCalendar = $getAllCalendar;
+    }
+
+    /**
+     * @Route("/calendar/management/edit/calendar", name="app_edit_calendar", methods={"GET"})
+     */
+    public function edit()
+    {
+        $adminUser = $this->getUser();
+        $company = $adminUser->getCompany();
+
+        $getAllCalendar = $this->getAllCalendar;
+        $calendarCollectionResponse = $getAllCalendar->__invoke($company);
+
+        return $this->render('calendar_management/edit_calendar.html.twig', [
+            'calendar_by_working_year' => $calendarCollectionResponse,
+        ]);
+    }
+}
