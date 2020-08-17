7f1d94a354a4e6b059469c4ae24548d7aa5c64ff
JesusCH92 <jcocaharo@hotmail.com>
2020-08-17 21:25:47 +0200

    create service for get all calendar, only get back working year and calendar id

---
+++ src/Calendar/ApplicationService/GetAllCalendar.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Calendar\ApplicationService;
+
+use App\Calendar\ApplicationService\DTO\CalendarByWorkingYearResponse;
+use App\Calendar\Domain\CalendarByWorkingYearRepository;
+use App\Entity\Company;
+
+final class GetAllCalendar
+{
+    private CalendarByWorkingYearRepository $calendarByWorkingYearRepository;
+
+    public function __construct(CalendarByWorkingYearRepository $calendarByWorkingYearRepository)
+    {
+        $this->calendarByWorkingYearRepository = $calendarByWorkingYearRepository;
+    }
+
+    public function __invoke(Company $company): array
+    {
+        $calendarCollection = $this->calendarByWorkingYearRepository->getAllCalendarByCompany($company);
+        $calendarResponseCollection = $this->mappingCalendarEntitiesCollectionToCalendarByWorkingYearResponse($calendarCollection);
+        
+        return $calendarResponseCollection;
+    }
+
+    public function mappingCalendarEntitiesCollectionToCalendarByWorkingYearResponse(array $calendarEntitiesCollection): array
+    {
+        $calendarResponseCollection = [];
+        foreach($calendarEntitiesCollection as $calendarEntity){
+            $calendarResponse = new CalendarByWorkingYearResponse(
+                $calendarEntity->calendarId(),
+                $calendarEntity->workingYear()->workingYear()
+            );
+            array_push($calendarResponseCollection, $calendarResponse);
+        }
+
+        return $calendarResponseCollection;
+    }
+}
