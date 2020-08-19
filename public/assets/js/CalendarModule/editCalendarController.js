a9f536ca9ac01043d0fe6ab116bd0d443bdfd804
JesusCH92 <jcocaharo@hotmail.com>
2020-08-19 05:25:12 +0200

    create controller for get calendar config by working year

---
+++ public/assets/js/CalendarModule/editCalendarController.js
@@ -0,0 +1,28 @@
+var editCalendarController = (function(){
+    var $workingYearFilterBtn = $("#working-year-filter-btn");
+    var $workingYearSelect = $("#working-year-select");
+    var $calendarConfigContainer = $("#calendar-config-container");
+
+    var paintCalendarConfig = function( {calendarConfigContainer, calendarConfig} ) {
+        calendarConfigContainer.html(calendarConfig);
+        $("#work-days-select").selectpicker();
+    };
+
+    var initEvent = function () {
+        $workingYearFilterBtn.click(function() {
+            var $workingYear = $workingYearSelect.val();
+            console.log('working year selected: ' + $workingYear);
+            if ($workingYear === "") {
+                return;
+            }
+
+            editCalendarModel.getCalendarConfigByWorkingYear({
+                id : $workingYear,
+                callback : paintCalendarConfig,
+                container : $calendarConfigContainer
+            });
+        });
+    };
+
+    initEvent();
+})();
