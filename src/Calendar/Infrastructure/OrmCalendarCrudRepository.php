862500bed7379e140d3c96aabd6a092bf613b1f8
JesusCH92 <jcocaharo@hotmail.com>
2020-08-20 05:25:30 +0200

    add (prepare) class for make CRUD in Calendar entity

---
+++ src/Calendar/Infrastructure/OrmCalendarCrudRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Calendar\Infrastructure;
+
+use App\Calendar\Domain\DayOffConfigRepository;
+use App\Entity\Calendar;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class OrmCalendarCrudRepository implements DayOffConfigRepository
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getCalendarByCalendarId(string $calendarId): ?Calendar
+    {
+        $calendarRepository = $this->entityManager->getRepository(Calendar::class);
+        $calendarEntity = $calendarRepository->find($calendarId);
+
+        return $calendarEntity;
+    }
+
+    public function saveCalendar(Calendar $calendarEntity): void
+    {
+        $this->entityManager->persist($calendarEntity);
+        $this->entityManager->flush();
+    }
+}
