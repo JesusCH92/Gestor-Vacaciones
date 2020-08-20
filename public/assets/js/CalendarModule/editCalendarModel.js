d169ac9c7715467b3de3c4ed36c55e83db0fd655
JesusCH92 <jcocaharo@hotmail.com>
2020-08-20 04:32:43 +0200

    add updateDayOffRequest method for send init date day off request and end date day off request by calendar id and update

--- public/assets/js/CalendarModule/editCalendarModel.js
+++ public/assets/js/CalendarModule/editCalendarModel.js
@@ -6,7 +6,22 @@ var editCalendarModel = (function() {
             async: true,
             dataType: "json",
             success: function(data){
-                callback({calendarConfigContainer : container, calendarConfig : data.calendar_config});
+                callback({calendarConfigContainer : container, calendarConfig : data.calendar_config, calendarId: id});
+            },
+            error: function(data){
+                console.log(data);
+            }
+        });
+    };
+
+    var updateDayOffRequest = function ({dayOffRequest, callback = console.log}) {
+        $.ajax({
+            type: 'PUT',
+            url: '/calendar/management/dayoffrequest',
+            async: true,
+            data: {dayOffRequest},
+            success: function(data){
+                callback(data);
             },
             error: function(data){
                 console.log(data);
@@ -15,6 +30,7 @@ var editCalendarModel = (function() {
     };
 
     return {
-        getCalendarConfigByWorkingYear : getCalendarConfigByWorkingYear
+        getCalendarConfigByWorkingYear : getCalendarConfigByWorkingYear,
+        updateDayOffRequest : updateDayOffRequest
     }
 });
