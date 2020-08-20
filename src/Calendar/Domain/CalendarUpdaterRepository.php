c79ac1b7e46c58494aad71e1fffbcdfe8e029965
JesusCH92 <jcocaharo@hotmail.com>
2020-08-20 11:46:29 +0200

    rename Repository from DayOffConfigRepository to CalendarUpdaterRepository

--- src/Calendar/Domain/CalendarUpdaterRepository.php
+++ src/Calendar/Domain/CalendarUpdaterRepository.php
@@ -6,7 +6,7 @@ namespace App\Calendar\Domain;
 
 use App\Entity\Calendar;
 
-interface DayOffConfigRepository
+interface CalendarUpdaterRepository
 {
     public function getCalendarByCalendarId(string $calendarId): ?Calendar;
     public function saveCalendar(Calendar $calendarEntity): void;
