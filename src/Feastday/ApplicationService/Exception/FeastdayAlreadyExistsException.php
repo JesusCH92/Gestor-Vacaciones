0b43731e41369f0a892a2308d27aa3680a674506
JesusCH92 <jcocaharo@hotmail.com>
2020-08-21 08:24:46 +0200

    create exception if try create a feastday already exists in calendar

---
+++ src/Feastday/ApplicationService/Exception/FeastdayAlreadyExistsException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Feastday\ApplicationService\Exception;
+
+use Exception;
+use Throwable;
+
+final class FeastdayAlreadyExistsException extends Exception
+{
+    public function __construct(string $feastdayDate, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct(sprintf('there is already a date like "%s" in calendar', $feastdayDate), $code, $previous);
+    }
+}
