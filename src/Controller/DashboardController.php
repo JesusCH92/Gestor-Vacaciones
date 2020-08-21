fee77c3f1645f733201075cd6a5713aff48baf8b
JesusCH92 <jcocaharo@hotmail.com>
2020-08-21 21:31:56 +0200

    hardcoding for render calendar with workings days, feastday

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
+use App\Calendar\ApplicationService\DTO\CalendarConfigRequest;
+use App\Calendar\ApplicationService\GetCalendarConfig;
+use App\DayOff\ApplicationService\GetDatesOfCalendar;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
+    private GetDatesOfCalendar $getDatesOfCalendar;
+    private GetCalendarConfig $getCalendarConfig;
+
+    public function __construct(GetDatesOfCalendar $getDatesOfCalendar, GetCalendarConfig $getCalendarConfig)
+    {
+        $this->getDatesOfCalendar = $getDatesOfCalendar;
+        $this->getCalendarConfig = $getCalendarConfig;
+    }
+    
     /**
      * @Route("/dashboard", name="app_dashboard")
      */
     public function index()
     {
+        $calendarId = "103f4127-598b-42a1-b672-75999ef7a763";
+        $calendar = new CalendarConfigRequest($calendarId);
+
+        $getCalendarConfig =$this->getCalendarConfig;
+        $calendarConfigResponse = $getCalendarConfig->__invoke($calendar);
+
+        $getDatesOfCalendar = $this->getDatesOfCalendar;
+        $calendarInfo = $getDatesOfCalendar->__invoke(
+            new DateTimeImmutable( $calendarConfigResponse->initDate() ),
+            new DateTimeImmutable(  $calendarConfigResponse->endDate() ),
+            $calendarConfigResponse->feastdayCollection()
+        );
+
         return $this->render('dashboard/dashboard.html.twig', [
-            'controller_name' => 'DashboardController',
+            'calendar_info' => $calendarInfo,
+            'working_days'  =>$calendarConfigResponse->workDays()
         ]);
     }
 }
