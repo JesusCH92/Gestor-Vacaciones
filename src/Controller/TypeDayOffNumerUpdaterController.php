fa4a059451d8c771ca6066ffdd358e498b8a4f4f
JesusCH92 <jcocaharo@hotmail.com>
2020-08-21 03:35:55 +0200

    create controller for update number of type day off

---
+++ src/Controller/TypeDayOffNumerUpdaterController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use App\TypeDayOff\ApplicationService\DTO\TypeDayOffRequest;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\TypeDayOff\ApplicationService\TypeDayOffNumberUpdater;
+
+final class TypeDayOffNumerUpdaterController extends AbstractController
+{
+    private TypeDayOffNumberUpdater $typeDayOffNumberUpdater;
+
+    public function __construct(TypeDayOffNumberUpdater $typeDayOffNumberUpdater)
+    {
+        $this->typeDayOffNumberUpdater = $typeDayOffNumberUpdater;
+    }
+
+    /**
+     * @Route("/calendar/management/typeDayOff", name="app_typeDayOff_update", methods={"PUT"})
+     */
+    public function typeDayOffNumberUpdater(Request $request)
+    {
+        $typeDayOff = $request->get('typeDayOff');
+        $calendarId = $typeDayOff['calendarId'];
+        $type = $typeDayOff['type'];
+        $number = $typeDayOff['number'];
+
+        $typeDayOffRequest = new TypeDayOffRequest(
+            $calendarId,
+            $type,
+            $number
+        );
+
+        $typeDayOffNumberUpdater = $this->typeDayOffNumberUpdater;
+        $typeDayOffNumberUpdater->__invoke($typeDayOffRequest);
+
+        return new Response('type day off number update');
+    }
+}
