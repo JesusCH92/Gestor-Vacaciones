70bdb88cf0040331a4e350d093e639d92b444ad2
JesusCH92 <jcocaharo@hotmail.com>
2020-08-21 08:28:26 +0200

    update class by implements FeastdayRepository an reuse getCalendarByCalendarId method for get caledar entity by calendar id

--- src/Calendar/Infrastructure/OrmCalendarCrudRepository.php
+++ src/Calendar/Infrastructure/OrmCalendarCrudRepository.php
@@ -6,9 +6,12 @@ namespace App\Calendar\Infrastructure;
 
 use App\Calendar\Domain\CalendarUpdaterRepository;
 use App\Entity\Calendar;
+use App\Entity\FeastDay;
+use App\Feastday\Domain\FeastdayRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 
-final class OrmCalendarCrudRepository implements CalendarUpdaterRepository
+final class OrmCalendarCrudRepository implements CalendarUpdaterRepository, FeastdayRepository
 {
     private EntityManagerInterface $entityManager;
 
@@ -28,6 +31,27 @@ final class OrmCalendarCrudRepository implements CalendarUpdaterRepository
     public function saveCalendar(Calendar $calendarEntity): void
     {
         $this->entityManager->persist($calendarEntity);
+        $this->entityManager->flush();
+    }
+
+    public function findFeastday(string $calendarId, DateTimeImmutable $feastdaydate): ?FeastDay
+    {
+        $feastdayRepository = $this->entityManager->getRepository(Feastday::class);
+        $feastday = $feastdayRepository->findBy(
+            [
+                'calendar'     => $calendarId,
+                'feastdayDate.feastdayDate' => $feastdaydate
+            ]
+        );
+
+        $feastdayEntity = [] === $feastday ? null : $feastday[0];
+
+        return $feastdayEntity;
+    }
+
+    public function saveFeastday(FeastDay $feastdayEntity): void
+    {
+        $this->entityManager->persist($feastdayEntity);
         $this->entityManager->flush();
     }
 }
