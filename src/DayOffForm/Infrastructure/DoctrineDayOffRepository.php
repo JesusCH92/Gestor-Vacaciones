293929e3c3ddfe148b3807904c08f6ba3cf4e24a
miriam <ls28958@salleurl.edu>
2020-08-26 00:15:01 +0200

    implement method findByUserAndStatusDayOffForm for search the count fo days selected in each request by user (if its approved or pending) and refactor the methods of persists in db

--- src/DayOffForm/Infrastructure/DoctrineDayOffRepository.php
+++ src/DayOffForm/Infrastructure/DoctrineDayOffRepository.php
@@ -18,21 +18,48 @@ final class DoctrineDayOffRepository implements DayOffRepository
     {
         $this->entityManager = $entityManager;
     }
+
     public function findOne(User $user)
     {
         $dayOffRepository = $this->entityManager->getRepository(DayOffForm::class);
         return $dayOffRepository->findBy(['idUser' => $user]);
     }
 
-    public function saveDayOffForm(DayOffForm $dayOffForm)
+    public function findByUserAndStatusDayOffForm(User $user, string $typeDayOffForm): array
+    {
+        $query = $this
+            ->entityManager
+            ->createQuery(
+                <<<DQL
+SELECT d.countDayOffRequest.countDayOffRequest
+FROM App\Entity\DayOffForm d
+WHERE d.user = :user AND d.typeDayOff = :type AND (d.statusDayOffForm.statusDayOffForm = :approved OR d.statusDayOffForm.statusDayOffForm = :pending )
+DQL
+            )->setParameters(
+                [
+                    'user' => $user,
+                    'type' => $typeDayOffForm,
+                    'approved' => 'APPROVED',
+                    'pending' => 'PENDING'
+                ]
+            );
+        return $query->getResult();
+    }
+
+    public function saveDayOffForm(DayOffForm $dayOffForm, array $dayOffFormRequestCollection): void
     {
         $this->entityManager->persist($dayOffForm);
         $this->entityManager->flush();
+
+        $this->saveDayOffFormRequest($dayOffFormRequestCollection);
+
     }
 
-    public function saveDayOffFormRequest(DayOffFormRequest $dayOffFormRequest)
+    public function saveDayOffFormRequest(array $dayOffFormRequestCollection) :void
     {
-        $this->entityManager->persist($dayOffFormRequest);
-        $this->entityManager->flush();
+        foreach ($dayOffFormRequestCollection as $dayOffFormRequest) {
+            $this->entityManager->persist($dayOffFormRequest);
+            $this->entityManager->flush();
+        }
     }
 }
