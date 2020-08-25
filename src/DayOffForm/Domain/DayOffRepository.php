33e6ecd7b9bbef447a90607b1c94eec0f8c4bd33
miriam <ls28958@salleurl.edu>
2020-08-26 00:13:02 +0200

    add parameter of type of day to findByUserAndStatusDayOffForm

--- src/DayOffForm/Domain/DayOffRepository.php
+++ src/DayOffForm/Domain/DayOffRepository.php
@@ -3,14 +3,12 @@
 
 namespace App\DayOffForm\Domain;
 
-
 use App\Entity\DayOffForm;
-use App\Entity\DayOffFormRequest;
 use App\User\Domain\User;
 
 interface DayOffRepository
 {
     public function findOne(User $userId);
-    public function saveDayOffForm(DayOffForm $dayOffForm, array $dayOffFormRequestCollection);
-    public function findByUserAndStatusDayOffForm(User $user);
+    public function saveDayOffForm(DayOffForm $dayOffForm, array $dayOffFormRequestCollection): void;
+    public function findByUserAndStatusDayOffForm(User $user, string $typeDayOffForm): array;
 }
