767dbeb59098a578dd1f53c7ff42ca599e336488
miriam <ls28958@salleurl.edu>
2020-08-27 00:47:53 +0200

    update DayOffFormController to get the calendar working years and render the template for filter by the year

--- src/Controller/DayOffFormController.php
+++ src/Controller/DayOffFormController.php
@@ -4,20 +4,17 @@
 namespace App\Controller;
 
 use App\Calendar\ApplicationService\GetCalendarByYear;
-use App\DayOffForm\ApplicationService\GetDatesOfCalendar;
-use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class DayOffFormController extends AbstractController
 {
     private GetCalendarByYear $getCalendarByYear;
-    private GetDatesOfCalendar $getDatesOfCalendar;
 
-    public function __construct(GetCalendarByYear $getCalendarByYear, GetDatesOfCalendar $getDatesOfCalendar)
-    {
+    public function __construct(
+        GetCalendarByYear $getCalendarByYear
+    ) {
         $this->getCalendarByYear = $getCalendarByYear;
-        $this->getDatesOfCalendar = $getDatesOfCalendar;
     }
 
     /**
@@ -25,28 +22,15 @@ final class DayOffFormController extends AbstractController
      */
     public function index()
     {
+        $user = $this->getUser();
 
         $calendarByYear = $this->getCalendarByYear;
         $year = date("Y");
-        $calendarConfigResponse = $calendarByYear->__invoke($year);
+        $calendar = $calendarByYear->__invoke($year);
 
-        $getDatesOfCalendar = $this->getDatesOfCalendar;
 
-        // $feastday = [];
-        // foreach($calendarConfigResponse->feastdayCollection() as $feastdayDate) {
-        //     array_push($feastday, $feastdayDate['date']);
-        // }
-        $calendarInfo = $getDatesOfCalendar->__invoke(
-            new DateTimeImmutable( $calendarConfigResponse->initDate() ),
-            new DateTimeImmutable(  $calendarConfigResponse->endDate() ),
-            $calendarConfigResponse->feastdayCollection()
-        );
-
-        return $this->render('dayoff_form/dayoff_form_request/dayoff.html.twig', [
-            'year' => $year,
-            'calendar' => $calendarConfigResponse,
-            'calendar_info' => $calendarInfo,
-            'working_days'  =>$calendarConfigResponse->workDays()
+        return $this->render('dayoff_form/dayoff.html.twig', [
+            'calendars' => $calendar
         ]);
 
     }
