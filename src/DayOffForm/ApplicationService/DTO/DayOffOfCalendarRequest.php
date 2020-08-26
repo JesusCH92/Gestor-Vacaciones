ffc6c8f34498856329eb77a279ae789cf6980e85
miriam <ls28958@salleurl.edu>
2020-08-27 00:57:53 +0200

    create DTO with the entity of Calendar, User and an array of types of day off used to pass info to the AS

---
+++ src/DayOffForm/ApplicationService/DTO/DayOffOfCalendarRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\DayOffForm\ApplicationService\DTO;
+
+
+use App\Entity\Calendar;
+use App\User\Domain\User;
+
+final class DayOffOfCalendarRequest
+{
+    private Calendar $calendar;
+    private User $user;
+    private array $typeDayOffCollection;
+
+    public function __construct(Calendar $calendar, User $user, array $typeDayOffCollection)
+    {
+        $this->calendar = $calendar;
+        $this->user = $user;
+        $this->typeDayOffCollection =$typeDayOffCollection;
+    }
+
+    public function calendar(): Calendar
+    {
+        return $this->calendar;
+    }
+
+    public function user(): User
+    {
+        return $this->user;
+    }
+
+    public function typeDayOffCollection(): array
+    {
+        return $this->typeDayOffCollection;
+    }
+}
