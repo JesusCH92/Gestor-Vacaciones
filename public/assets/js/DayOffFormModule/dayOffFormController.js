5f12cf869b66a80f3eb2bac8c7e0808746914cca
miriam <ls28958@salleurl.edu>
2020-08-28 01:54:32 +0200

    update dayOffFormController for check the dates selected and print it

--- public/assets/js/DayOffFormModule/dayOffFormController.js
+++ public/assets/js/DayOffFormModule/dayOffFormController.js
@@ -1,7 +1,17 @@
 var dayOffFormController = (function(_calendarId){
-    var calendarId = _calendarId;
+    var $calendarId = _calendarId;
     var $CarouselDates = $("#datesCarousel");
     var $datesSelectedCalendar = $(".dates-selected-calendar");
+    var $countDaysSelected = $(".count-days-selected");
+    var $dateRangeSelected = $(".date-range");
+    var $initRange = $(".init-range");
+    var $endRange = $(".end-range");
+    var $initDateDayOff = $("#init-date-day-off");
+    var $endDateDayOff = $("#end-date-day-off");
+    var $feastDaysCalendarCollection = $("#feast-days-calendar-collection");
+    var $workingDaysCalendarCollection = $("#work-days-calendar-collection");
+    var $saveDayOffForm = $("#save-day-off-form");
+
     var initEventDayOffConfig = function() {
         $CarouselDates.click( function() {
             if (!$(event.target).hasClass('selectable-day')) {
@@ -9,12 +19,120 @@ var dayOffFormController = (function(_calendarId){
             }
 
             var $dayOfMonth = $(event.target).attr("date");
-            var dateList =   document.createElement("li");
-            dateList.className = "list-group-item date selected";
-            dateList.innerText = $dayOfMonth;
-            $datesSelectedCalendar.append(dateList);
-            console.log($dayOfMonth);
+            printDates($dayOfMonth);
         });
+
+
+        $dateRangeSelected.change(function() {
+
+            if ("" === $initRange.val() || "" === $endRange.val()){
+                return;
+            }
+            if (isValidDate($initRange.val()) !== true ) {
+                console.log('no es valida la fecha');
+                return;
+            }
+            if ($initRange.val() < $initDateDayOff.html() || $endRange.val() > $endDateDayOff.html() || $endRange.val()<$initRange.val()){
+                console.log("La fecha seleccionada es incorrecta");
+                //alert("La fecha seleccionada es menor a la permitida");
+            }
+            $feastDays=JSON.parse($feastDaysCalendarCollection.val());
+
+            $arrayDatesRange = getDatesInRange($initRange.val(),$endRange.val());
+            for (let i=0;i<$arrayDatesRange.length;i++){
+                printDates($arrayDatesRange[i]);
+            }
+
+
+        });
+
+        $saveDayOffForm.click( function() {
+            $datesSelectedArray = getDatesSelected();
+            $typeDayOff =$("#type-day-off-form-select").children("option:selected").val();
+
+            //var _dayoffFormConfigModel = dayOffFormConfigModel();
+            saveDayOffFormRequest({
+                day_off_request : {
+                    id_calendar : $calendarId,
+                    days_off : JSON.stringify($datesSelectedArray),
+                    type_of_day : $typeDayOff
+                }
+            });
+
+
+        });
+
+        var saveDayOffFormRequest = function ({day_off_request, callback = console.log}) {
+            $.ajax({
+                type: 'POST',
+                url: '/dayoff/management/add',
+                async: true,
+                data: {day_off_request},
+                success: function(data){
+                    callback(data);
+                },
+                error: function(data){
+                    console.log(JSON.parse(data.responseText));
+                }
+            });
+        };
+
+
+        var printDates= function ($date){
+            $datesSelectedArray = getDatesSelected();
+
+            $feastDays=JSON.parse($feastDaysCalendarCollection.val());
+            $workingDays = JSON.parse($workingDaysCalendarCollection.val());
+            var currentDay = new Date($date).getDay().toString();
+
+            if (!$feastDays.includes($date) && $workingDays.includes(currentDay) && !$datesSelectedArray.includes($date)){
+                var dateList =   document.createElement("li");
+                dateList.className = "list-group-item date-selected";
+                dateList.innerText = $date;
+                $datesSelectedCalendar.append(dateList);
+
+                var countDaysSelected = $countDaysSelected.html();
+                var countdays =countDaysSelected.split(":");
+                var count =parseInt(countdays[1])+1;
+                $countDaysSelected.html("Días seleccionados:"+ count);
+            }
+
+        };
+        var getDatesInRange =function (startDate, stopDate) {
+            var dateArray = [];
+            var currentDate = new Date(startDate);
+            var endDate = new Date(stopDate);
+            while (currentDate <= endDate) {
+                var month = parseInt(currentDate.getMonth())+1;
+                var day = currentDate.getDate();
+                if (month.toString().length<2){
+                    month="0"+month.toString();
+                }
+                if (day.toString().length<2){
+                    day="0"+day.toString();
+                }
+                dateArray.push( currentDate.getFullYear()+"-"+month +"-"+day );
+                currentDate.setDate(currentDate.getDate() + 1);
+
+            }
+            return dateArray;
+        };
+        var getDatesSelected = function () {
+            var dateArray = [];
+            $listDatesSelected = $datesSelectedCalendar.find("li");
+            for (let i=0; i<$listDatesSelected.length;i++){
+                //console.log($listDatesSelected[i].innerText);
+                dateArray.push( $listDatesSelected[i].innerText );
+            }
+            //$dateSelected = $listDatesSelected.html();
+            //console.log($listDatesSelected);
+            return dateArray;
+        }
+        var isValidDate = function ($dateString) {
+            var $date = new Date($dateString);
+            return $date instanceof Date && !isNaN($date);
+        };
+
     };
 
     return{
