46be6e6fb06d5a11cf96d849b34523d4b602df81
JesusCH92 <jcocaharo@hotmail.com>
2020-08-28 16:04:24 +0200

    create service for register User

---
+++ src/User/ApplicationService/RegisterUser.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\User\ApplicationService;
+
+use App\User\ApplicationService\DTO\RegisterUserRequest;
+use App\User\Domain\Factory\UserFactory;
+use App\User\Domain\UserRepository;
+
+final class RegisterUser
+{
+    private UserFactory $userFactory;
+
+    private UserRepository $userRepository;
+
+    public function __construct(UserFactory $userFactory, UserRepository $userRepository)
+    {
+        $this->userFactory = $userFactory;
+        $this->userRepository = $userRepository;
+    }
+
+    public function __invoke(RegisterUserRequest $registerUserRequest)
+    {
+        $email = $registerUserRequest->email();
+
+        if (null !== $this->userRepository->findUserByEmail($email)) {
+            // ! lanzar una excepcion
+        }
+
+        $user = $this->userFactory->register(
+            $registerUserRequest->name(),
+            $registerUserRequest->lastName(),
+            $registerUserRequest->phone(),
+            $email,
+            $registerUserRequest->password(),
+            $registerUserRequest->deparment(),
+            $registerUserRequest->company(),
+            $registerUserRequest->roles()
+        );
+
+        $this->userRepository->saveUser($user);
+    }
+}
