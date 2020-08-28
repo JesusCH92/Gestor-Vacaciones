6f69373dd15135babca62cd38ee0a8354359dbd0
miriam <ls28958@salleurl.edu>
2020-08-28 17:59:39 +0200

    add service to calculate the reamining days by type

--- src/Controller/DayOffRequestController.php
+++ src/Controller/DayOffRequestController.php
@@ -5,8 +5,12 @@ namespace App\Controller;
 use App\Calendar\ApplicationService\DTO\CalendarConfigRequest;
 use App\Calendar\ApplicationService\GetCalendarById;
 use App\Calendar\ApplicationService\GetCalendarConfig;
+use App\DayOffForm\ApplicationService\DTO\RemainingDaysOffRequest;
 use App\DayOffForm\ApplicationService\DTO\DayOffRequest;
+use App\DayOffForm\ApplicationService\GetRemainingDaysOffByUser;
 use App\DayOffForm\ApplicationService\SaveDayOffRequest;
+use App\Entity\Calendar;
+use App\User\Domain\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,12 +21,18 @@ final class DayOffRequestController extends AbstractController
     private SaveDayOffRequest $saveDayOffRequest;
     private GetCalendarConfig $getCalendarConfig;
     private GetCalendarById $getCalendarById;
+    private GetRemainingDaysOffByUser $getRemainingDaysOffByUser;
 
-    public function __construct(SaveDayOffRequest $saveDayOffRequest, GetCalendarById $getCalendarById,GetCalendarConfig $getCalendarConfig)
-    {
+    public function __construct(
+        SaveDayOffRequest $saveDayOffRequest,
+        GetCalendarById $getCalendarById,
+        GetCalendarConfig $getCalendarConfig,
+        GetRemainingDaysOffByUser $getRemainingDaysOffByUser
+    ) {
         $this->saveDayOffRequest = $saveDayOffRequest;
         $this->getCalendarById = $getCalendarById;
         $this->getCalendarConfig = $getCalendarConfig;
+        $this->getRemainingDaysOffByUser = $getRemainingDaysOffByUser;
     }
 
     /**
@@ -40,8 +50,10 @@ final class DayOffRequestController extends AbstractController
         $getCalendarById = $this->getCalendarById;
         $calendar = $getCalendarById->__invoke($calendarRequest);
 
-        $getCalendarConfig =$this->getCalendarConfig;
+        $getCalendarConfig = $this->getCalendarConfig;
         $calendarConfigResponse = $getCalendarConfig->__invoke($calendarRequest);
+
+        $remainingDaysByType = $this->remainingDays($calendar->calendar(), $user, $calendarConfigResponse->typeDayOffCollection(),$request['type_of_day']);
 
         $saveDayOffRequest = $this->saveDayOffRequest;
         $saveDayOffRequest(
@@ -51,10 +63,20 @@ final class DayOffRequestController extends AbstractController
                 $request['type_of_day'],
                 json_decode($request['days_off']),
                 $calendarConfigResponse->typeDayOffCollection(),
-                $calendarConfigResponse->feastdayCollection()
+                $calendarConfigResponse->feastdayCollection(),
+                $remainingDaysByType
             )
         );
 
         return Response::create('??');
+    }
+
+    private function remainingDays(Calendar $calendar, User $user, array $typeDayOffCollection, string $typeDayOff) :int
+    {
+        $dayOffOfCalendarRequest = new RemainingDaysOffRequest($calendar, $user,
+            $typeDayOffCollection);
+        $getRemainingDaysOffByUser = $this->getRemainingDaysOffByUser;
+        $remainingDaysOffResponse = $getRemainingDaysOffByUser->__invoke($dayOffOfCalendarRequest);
+        return $remainingDaysOffResponse->remainingDaysOff()[$typeDayOff];
     }
 }
