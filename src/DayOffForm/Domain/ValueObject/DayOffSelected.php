cb67fcc5f257a616553916cced302b2f8f7dc773
miriam <ls28958@salleurl.edu>
2020-08-28 19:52:44 +0200

    delete unused methods

--- src/DayOffForm/Domain/ValueObject/DayOffSelected.php
+++ src/DayOffForm/Domain/ValueObject/DayOffSelected.php
@@ -17,11 +17,7 @@ final class DayOffSelected
      */
     private $dayOffSelected;
 
-    /**
-     * DayOffSelected constructor.
-     * @param $dayOffSelected
-     * @throws Exception
-     */
+
     public function __construct(string $dayOffSelected)
     {
         $this->checkCorrectDateType($dayOffSelected);
@@ -31,18 +27,12 @@ final class DayOffSelected
     private function checkCorrectDateType(string $dayOffSelected)
     {
         $date = explode('-', $dayOffSelected);
+
         if (!checkdate($date[1], $date[2], $date[0])) {
             throw new InvalidDateTypeException();
         }
-        /*$this->checkCorrectYearType($date[0]);
-        $this->checkCorrectMonthType($date[1]);
-        $this->checkCorrectDayType($date[2]);
-        */
     }
 
-    /**
-     * @return mixed
-     */
     public function dayOffSelected()
     {
         return $this->dayOffSelected;
@@ -52,27 +42,6 @@ final class DayOffSelected
     {
         if ($initDate > $this->dayOffSelected || $endDate < $this->dayOffSelected) {
             throw new InvalidDayOffSelectedException($initDate, $endDate);
-        }
-    }
-
-    private function checkCorrectYearType(string $year): void
-    {
-        if (!4 === strlen($year) || date("Y") > date($year)) {
-            throw new InvalidDateTypeException();
-        }
-    }
-
-    private function checkCorrectMonthType(string $month): void
-    {
-        if (!2 === strlen($month) || 12 < (int)$month) {
-            throw new InvalidDateTypeException();
-        }
-    }
-
-    private function checkCorrectDayType(string $day): void
-    {
-        if (!2 === strlen($day) || 31 < (int)$day) {
-            throw new InvalidDateTypeException();
         }
     }
 
