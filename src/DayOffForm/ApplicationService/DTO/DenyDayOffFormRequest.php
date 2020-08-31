07bf13fe993b59259e2e8b4c6412bb57b94c6df0
miriam <ls28958@salleurl.edu>
2020-08-31 16:17:13 +0200

    create AS for deny the dayOffForm of the employee

---
+++ src/DayOffForm/ApplicationService/DTO/DenyDayOffFormRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\DayOffForm\ApplicationService\DTO;
+
+
+class DenyDayOffFormRequest
+{
+    private string $dayOffFormId;
+    private string $supervisorId;
+    private string $comment;
+
+    public function __construct(string $dayOffFormId, string $supervisorId, string $comment)
+    {
+        $this->dayOffFormId = $dayOffFormId;
+        $this->supervisorId = $supervisorId;
+        $this->comment = $comment;
+    }
+
+    public function dayOffFormId(): string
+    {
+        return $this->dayOffFormId;
+    }
+
+    public function supervisorId(): string
+    {
+        return $this->supervisorId;
+    }
+
+    public function comment(): string
+    {
+        return $this->comment;
+    }
+}
