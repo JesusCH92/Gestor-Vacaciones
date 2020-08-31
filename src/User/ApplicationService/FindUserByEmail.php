f20ba711ae880b64dd697ade840954d4f93c223b
JesusCH92 <jcocaharo@hotmail.com>
2020-08-31 16:52:49 +0200

    create service for find user by email, return entity or null

---
+++ src/User/ApplicationService/FindUserByEmail.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\User\ApplicationService;
+
+use App\User\Domain\UserRepository;
+
+final class FindUserByEmail
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function __invoke(string $email)
+    {
+        return $this->userRepository->findUserByEmail($email);
+    }
+}
