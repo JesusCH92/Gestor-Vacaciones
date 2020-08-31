42a3faef02a4ff9cc284b5d3712706acd62f8693
miriam <ls28958@salleurl.edu>
2020-08-31 16:18:07 +0200

    create controller for deny the day off form of the employee

---
+++ src/Controller/DenyNotificationEmployeesController.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\DayOffForm\ApplicationService\DenyDayOffForm;
+use App\DayOffForm\ApplicationService\DTO\DenyDayOffFormRequest;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class DenyNotificationEmployeesController extends AbstractController
+{
+    private DenyDayOffForm $denyDayOffForm;
+
+    public function __construct(DenyDayOffForm $denyDayOffForm)
+    {
+        $this->denyDayOffForm = $denyDayOffForm;
+    }
+
+    /**
+     * @Route("/notification/supervisor/denied", options={"expose"=true}, name="app_notification_supervise_denied")
+     */
+    public function dayOff(Request $request)
+    {
+        $user = $this->getUser();
+
+        $request = $request->get('day_off');
+
+        $observation = $request['comment'];
+        $dayOffId = $request['day_off_id'];
+        $denyDayOffForm = $this->denyDayOffForm;
+        $denyDayOffForm->__invoke(new DenyDayOffFormRequest($dayOffId, $user->getId(), $observation));
+
+
+        return Response::create('??????');
+    }
+}
