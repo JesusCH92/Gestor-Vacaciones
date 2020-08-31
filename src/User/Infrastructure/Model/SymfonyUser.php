a32a9be3f6d74f903af6452333a26a97f54ba5ec
JesusCH92 <jcocaharo@hotmail.com>
2020-08-31 21:06:44 +0200

    update symfony user for get roles

--- src/User/Infrastructure/Model/SymfonyUser.php
+++ src/User/Infrastructure/Model/SymfonyUser.php
@@ -33,7 +33,7 @@ final class SymfonyUser extends User implements UserInterface
         // guarantee every user at least has ROLE_USER
         //$roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
+        return array_unique($roles->roles());
     }
 
     /**
