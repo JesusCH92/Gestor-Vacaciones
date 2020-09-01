0582f10b35236dc07b822fccc1816f49e214ed58
miriam <ls28958@salleurl.edu>
2020-09-01 20:52:41 +0200

    create implementation od DepartmentRepository

---
+++ src/Department/Infrastructure/OrmDepartmentRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Department\Infrastructure;
+
+
+use App\Department\Domain\DepartmentRepository;
+use App\Entity\Department;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class OrmDepartmentRepository implements DepartmentRepository
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getAllDepartment(): array
+    {
+        $department = $this->entityManager->getRepository(Department::class);
+        return $department->findAll();
+    }
+
+    public function getDepartmentByName(string $name)
+    {
+        $department = $this->entityManager->getRepository(Department::class);
+        return $department->findBy(['departmentName'=> $name]);
+    }
+}
