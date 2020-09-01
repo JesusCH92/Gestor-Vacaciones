a89e53d6aeaf5aa5a7d79789bea04fd8bd16e3e4
miriam <ls28958@salleurl.edu>
2020-09-01 20:39:08 +0200

    add service to get all department  to controller

--- src/Controller/SearchDayOffByUserController.php
+++ src/Controller/SearchDayOffByUserController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Department\ApplicationService\GetAllDepartments;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchDayOffByUserController extends AbstractController
 {
+    private GetAllDepartments $getAllDepartments;
+
+    public function __construct(GetAllDepartments $getAllDepartments)
+    {
+        $this->getAllDepartments = $getAllDepartments;
+    }
+
     /**
      * @Route("/supervise/dayoff/employees", name="supervise_day_off_employee")
      */
     public function index()
     {
-        return $this->render('supervise_employees/supervise_search_day_off/search-employee-day-off.twig', [
+        $getAllDepartments = $this->getAllDepartments;
+        $departmentCollection = $getAllDepartments->__invoke();
 
+        return $this->render('supervise_employees/supervise_search_day_off/search-employee-day-off.twig', [
+            'department' => $departmentCollection->departmentCollection()
         ]);
 
     }
