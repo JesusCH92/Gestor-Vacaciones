3b7177542f55ad2655e40c322ccd9083b07288f1
miriam <ls28958@salleurl.edu>
2020-09-03 00:03:49 +0200

    delete method of mapping and call the AS FindDatesDayOffFormByDepartmentByUser method of mapping to use it

--- src/DayOffForm/ApplicationService/FindDatesDayOffFormByUser.php
+++ src/DayOffForm/ApplicationService/FindDatesDayOffFormByUser.php
@@ -17,39 +17,12 @@ final class FindDatesDayOffFormByUser
         $this->usersInDayOffFormRepository = $usersInDayOffFormRepository;
     }
 
-    public function __invoke(UserInDayOffFormByUserIdRequest $userInDayOffFormByUserIdRequest)
+    public function __invoke(UserInDayOffFormByUserIdRequest $userInDayOffFormByUserIdRequest): array
     {
         $usersInDayOffApproved = $this->usersInDayOffFormRepository->findDayOffFormRequestByCalendarUserId($userInDayOffFormByUserIdRequest->calendar(),
             $userInDayOffFormByUserIdRequest->userId());
-        return $this->mappingUsersInDayOffCollection($usersInDayOffApproved);
+
+        return FindDatesDayOffFormByDepartmentByUserName::mappingUsersInDayOffCollection($usersInDayOffApproved);
     }
 
-    public function mappingUsersInDayOffCollection(array $usersInDayOffApproved): array
-    {
-        $usersInDayOffCollection = [];
-        $codeDayOffForm = array_unique(array_column($usersInDayOffApproved, 'codeDayOffForm'));
-
-        foreach ($codeDayOffForm as $code) {
-            $dayOffFormRequestByCode = array_filter($usersInDayOffApproved, function ($userInDayOff) use ($code) {
-                return ($code === $userInDayOff['codeDayOffForm']);
-            });
-
-            $daysOffFormRequest = [];
-            foreach ($dayOffFormRequestByCode as $dayOffFormByCode) {
-                array_push($daysOffFormRequest, $dayOffFormByCode['dayOffSelected.dayOffSelected']);
-            }
-
-            $userId = $dayOffFormRequestByCode[array_key_first($dayOffFormRequestByCode)]['userId'];
-            $email = $dayOffFormRequestByCode[array_key_first($dayOffFormRequestByCode)]['email'];
-            $name = $dayOffFormRequestByCode[array_key_first($dayOffFormRequestByCode)]['name'];
-            $lastName = $dayOffFormRequestByCode[array_key_first($dayOffFormRequestByCode)]['lastname'];
-            $dayOffFormId = $dayOffFormRequestByCode[array_key_first($dayOffFormRequestByCode)]['lastname'];
-
-            $userInDayOffFormResponse = new UserInDayOffFormResponse($userId, $email, $name,
-                $lastName, $dayOffFormId, $daysOffFormRequest);
-
-            array_push($usersInDayOffCollection, $userInDayOffFormResponse);
-        }
-        return $usersInDayOffCollection;
-    }
 }
