6af81a98c224a0f416353f65aabfe511b25716de
miriam <ls28958@salleurl.edu>
2020-09-02 19:56:43 +0200

    delete comments and add a callback function when the ajax petition success for print the template

--- public/assets/js/SuperviseEmployees/DaysOffEmployees/daysOffConfigController.js
+++ public/assets/js/SuperviseEmployees/DaysOffEmployees/daysOffConfigController.js
@@ -4,8 +4,12 @@ var daysOffConfigController = (function(){
     $departmentSelect = $('#filter-department');
     $filterUserDayOffContainer = $('.filter-user-day-off-container');
 
-    var paintFilterDayOff= function(filterUserDayOff, dayoffConfig){
+    var paintFilterDayOff= function({filterUserDayOff, dayoffConfig}){
         console.log("hola");
+        filterUserDayOff.html(dayoffConfig);
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
     }
 
     var _daysOffModel = daysOffModel();
@@ -20,40 +24,12 @@ var daysOffConfigController = (function(){
                 user: $userName
             };
             console.log($userInDayOffCorpus);
-            _daysOffModel.getDayOffFormByDepartment({ filterEmployeesCorpus : $userInDayOffCorpus });
-
-
-           /* if ($department === "Todos los departamentos" && !$filterName.val().trim()){
-                console.log("no department and name selected");
-                $department = "";
-                console.log($department);
-                getAllDayOffForm();
-                return;
-            }
-            if ($department !== "Todos los departamentos" && !$filterName.val().trim()){
-                console.log("department selected");
-                getDayOffFormByDepartment({filter_day_off:{
-                        department: $department}});
-                return;
-
-            }
-            if ($department === "Todos los departamentos" && $filterName.val().trim() !== ""){
-                console.log("name selected");
-                getDayOffFormByName({filter_day_off:{
-                        user_name: $userName
-                    }});
-                return;
-            }
-            getDayOffFormByDepartmentByName({
-                filter_day_off:{
-                    department: $department,
-                    user_name: $userName
-                },
+            _daysOffModel.getDayOffFormByDepartment({
+                filterEmployeesCorpus : $userInDayOffCorpus,
+                callback : paintFilterDayOff,
+                container : $filterUserDayOffContainer
             });
 
-
-
-            */
         });
 
 
