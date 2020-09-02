372ee62a696b5791793ef574410d274f838501b6
JesusCH92 <jcocaharo@hotmail.com>
2020-09-03 00:23:35 +0200

    refactor->replace paint method for MVC design pattern

--- public/assets/js/SuperviseEmployees/DaysOffEmployees/userInDayOffController.js
+++ public/assets/js/SuperviseEmployees/DaysOffEmployees/userInDayOffController.js
@@ -10,8 +10,7 @@ var userInDayOffController = (function(){
         $userInDayOffFiltered.click(function() {
             var $userId = $(this).attr('userId');
             console.log($userId);
-            _userInDayOffRenderTemplate.paintCalendarForUserInDayOffByCurrentYear({ calendarContainer : $calendarContainer, calendar : `hola: ${$userId}` });
-            //_daysOffModel.getCalendarWithUserInDayOffByCurrentYear({id : $userId, callback = console.log, container: $calendarContainer});
+            _daysOffModel.getCalendarWithUserInDayOffByCurrentYear({id : $userId, callback : _userInDayOffRenderTemplate.paintCalendarForUserInDayOffByCurrentYear, container: $calendarContainer});
         });
     };
 
