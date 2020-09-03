5ee0589908375787d264f1fc8d73a85620a93d1a
miriam <ls28958@salleurl.edu>
2020-09-03 12:29:38 +0200

    add methods of ajax petition to get the employee notification selected and approve or deny the day off request

--- public/assets/js/SuperviseEmployees/Notification/superviseEmployeesController.js
+++ public/assets/js/SuperviseEmployees/Notification/superviseEmployeesController.js
@@ -5,11 +5,13 @@ var superviseEmployeesController = (function(){
     var $dayOffFormId = $("#day-off-form-id");
 
     var initSuperviseEmployees = function() {
+        _superviseEmployeesModel = superviseEmployeesModel();
         $approveDayOffFormBtn.click(function (){
             console.log($dayOffFormId.attr("value"));
             $comment = $superviseComment.val();
             $dayOffId = $dayOffFormId.attr("value");
-            approveDayOffformRequest({day_off: {
+
+            _superviseEmployeesModel.approveDayOffformRequest({day_off: {
                 comment: $comment,
                 day_off_id: $dayOffId
                 }});
@@ -18,45 +20,12 @@ var superviseEmployeesController = (function(){
             console.log("denied");
             $comment = $superviseComment.val();
             $dayOffId = $dayOffFormId.attr("value");
-            denyDayOffformRequest({day_off: {
+            _superviseEmployeesModel.denyDayOffformRequest({day_off: {
                     comment: $comment,
                     day_off_id: $dayOffId
                 }});
         });
 
-        var approveDayOffformRequest = function (day_off_request, callback = console.log) {
-            $.ajax({
-                type: 'POST',
-                url: '/notification/supervisor/approved',
-                async: true,
-                data: day_off_request,
-                success: function(data){
-                    //window.location.replace("http://localhost:8080/notification/list/employees");
-                    location.reload();
-                    callback(data);
-                },
-                error: function(data){
-                    console.log(data.responseText);
-                }
-            });
-        };
-
-        var denyDayOffformRequest = function (day_off_request, callback = console.log) {
-            $.ajax({
-                type: 'POST',
-                url: '/notification/supervisor/denied',
-                async: true,
-                data: day_off_request,
-                success: function(data){
-                    //window.location.replace("http://localhost:8080/notification/list/employees");
-                    location.reload();
-                    callback(data);
-                },
-                error: function(data){
-                    console.log(data.responseText);
-                }
-            });
-        };
     };
 
     return{
