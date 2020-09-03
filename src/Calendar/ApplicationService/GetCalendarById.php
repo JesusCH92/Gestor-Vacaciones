8b23b2b6073eb3f15049c131a16be3b8c45fb3f7
miriam <ls28958@salleurl.edu>
2020-09-03 11:30:25 +0200

    add throw exception if the id entried is not and uuuid

--- src/Calendar/ApplicationService/GetCalendarById.php
+++ src/Calendar/ApplicationService/GetCalendarById.php
@@ -8,6 +8,7 @@ use App\Calendar\ApplicationService\DTO\CalendarByIdResponse;
 use App\Calendar\ApplicationService\DTO\CalendarConfigRequest;
 use App\Calendar\ApplicationService\Exception\CalendarNotFoundException;
 use App\Calendar\Domain\CalendarConfigRepository;
+use Ramsey\Uuid\Nonstandard\Uuid;
 
 class GetCalendarById
 {
@@ -17,8 +18,13 @@ class GetCalendarById
     {
         $this->calendarConfigRepository = $calendarConfigRepository;
     }
+
     public function __invoke(CalendarConfigRequest $calendarConfigRequest): CalendarByIdResponse
     {
+        if (!Uuid::isValid($calendarConfigRequest->calendarId())) {
+            throw new CalendarNotFoundException();
+        }
+
         $calendarEntity = $this->calendarConfigRepository->getCalendarByCalendarId($calendarConfigRequest);
 
         if (null === $calendarEntity) {
