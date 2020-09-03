0a0d6e8c5acdff5d1da274301c4c724383a2eca6
miriam <ls28958@salleurl.edu>
2020-09-03 12:28:59 +0200

    delete methods of ajax petition because now its on superviseEmployeesModel

--- public/assets/js/SuperviseEmployees/Notification/superviseEmployeesModel.js
+++ public/assets/js/SuperviseEmployees/Notification/superviseEmployeesModel.js
@@ -1,17 +1,47 @@
 var superviseEmployeesModel = (function() {
 
-    var getDayOffFormByUser = function ({dayOffRequest, callback = console.log, container}) {
-        console.log({dayOffRequest});
+    var getDayOffFormByUser = function ({dayOffFormId, callback = console.log, container}) {
         $.ajax({
             type: 'GET',
-            url: `/notification/list/employees/dayoff/${dayOffRequest}`,
+            url: `employees/dayoff/${dayOffFormId}`,
             async: true,
-            data: {dayOffRequest},
             success: function(data){
-                callback({notificationContainer : container, dayoffConfig : data.dayoff_config});
+                callback({employeeInfo : container, dayoffConfig : data.dayoff_config});
             },
             error: function(data){
                 console.log(data);
+            }
+        });
+    };
+    var approveDayOffformRequest = function ({day_off, callback = console.log}) {
+        $.ajax({
+            type: 'POST',
+            url: '/supervise/management/notification/approve',
+            async: true,
+            data: {day_off},
+            success: function(data){
+                //window.location.replace("http://localhost:8080/notification/list/employees");
+                location.reload();
+                callback(data);
+            },
+            error: function(data){
+                console.log(data.responseText);
+            }
+        });
+    };
+    var denyDayOffformRequest = function ({day_off, callback = console.log}) {
+        $.ajax({
+            type: 'POST',
+            url: '/supervise/management/notification/deny',
+            async: true,
+            data: {day_off},
+            success: function(data){
+                //window.location.replace("http://localhost:8080/notification/list/employees");
+                location.reload();
+                callback(data);
+            },
+            error: function(data){
+                console.log(data.responseText);
             }
         });
     };
@@ -37,6 +67,8 @@ var superviseEmployeesModel = (function() {
 
      */
     return {
-        getDayOffFormByUser : getDayOffFormByUser
+        getDayOffFormByUser : getDayOffFormByUser,
+        approveDayOffformRequest : approveDayOffformRequest,
+        denyDayOffformRequest : denyDayOffformRequest
     }
 });
