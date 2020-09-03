d18bb7ec493f705fc96b99558a8cc98c8ef06d58
miriam <ls28958@salleurl.edu>
2020-09-03 14:20:47 +0200

    refactor exception-> now do not have the attributes the init date and end date

--- src/DayOffForm/Domain/Exception/InvalidDayOffSelectedException.php
+++ src/DayOffForm/Domain/Exception/InvalidDayOffSelectedException.php
@@ -8,7 +8,7 @@ use Throwable;
 
 final class InvalidDayOffSelectedException extends Exception
 {
-    public function __construct($initDate, $endDate, $code = 0, Throwable $previous = null)
+    public function __construct($code = 0, Throwable $previous = null)
     {
         parent::__construct(
             "The day selected is not correct. Select the days between init date and end date",
