557be869c57c08e7462a3d22c8b807f196b67ccb
miriam <ls28958@salleurl.edu>
2020-09-03 12:50:14 +0200

    create exception if the date selected is lower than the current date

--- src/DayOffForm/Domain/Exception/InvalidCountDayOffRequest.php
+++ src/DayOffForm/Domain/Exception/InvalidCountDayOffRequest.php
@@ -11,7 +11,7 @@ final class InvalidCountDayOffRequest extends Exception
     public function __construct(int $remainingDays,$code = 0, Throwable $previous = null)
     {
         parent::__construct(
-            "The number of days selected is greater than the remaining days: $remainingDays",
+            "The number of days selected is greater than the remaining days",
             $code,
             $previous
         );
