1f074e61b5c2f9accbde708197532cd5f29766c1
JesusCH92 <jcocaharo@hotmail.com>
2020-09-08 22:26:10 +0200

    implements methods for find user by id and find users in department

--- src/User/Infrastructure/Persistence/OrmUserRepository.php
+++ src/User/Infrastructure/Persistence/OrmUserRepository.php
@@ -5,11 +5,13 @@ declare(strict_types = 1);
 namespace App\User\Infrastructure\Persistence;
 
 use App\User\Domain\User;
+use App\User\Domain\UserByDepartmentRepository;
+use App\User\Domain\UserByIdRepository;
 use App\User\Domain\UserRepository;
 use App\User\Infrastructure\Model\SymfonyUser;
 use Doctrine\ORM\EntityManagerInterface;
 
-final class OrmUserRepository implements UserRepository
+final class OrmUserRepository implements UserRepository, UserByDepartmentRepository, UserByIdRepository
 {
     private EntityManagerInterface $entityManager;
 
@@ -36,5 +38,29 @@ final class OrmUserRepository implements UserRepository
     {
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+    }
+
+    public function findUsersByDepartment(string $userName, string $departmentId): array
+    {
+        $qb = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('su')
+            ->from(SymfonyUser::class, 'su')
+            ->where('su.department = :department')
+            ->andWhere('su.name LIKE :username')
+            ->setParameter('department', $departmentId)
+            ->setParameter('username', "%$userName%")
+            ;
+            
+        return $qb->getQuery()->getResult();
+    }
+
+    public function findUserById(string $userId): ?User
+    {
+        $userRepository = $this->entityManager->getRepository(SymfonyUser::class);
+        $user = $userRepository->find($userId); // ! return entity or null
+
+        return $user;
     }
 }
