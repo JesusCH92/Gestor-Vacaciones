c7cd9ed0bb869c4c66d75c9898b24524953fd924
JesusCH92 <jcocaharo@hotmail.com>
2020-09-09 22:54:28 +0200

    update service for delete user, removing day off form request (first) and day off form (after)

--- src/User/ApplicationService/UserDeleter.php
+++ src/User/ApplicationService/UserDeleter.php
@@ -4,6 +4,11 @@ declare(strict_types = 1);
 
 namespace App\User\ApplicationService;
 
+use App\DayOffForm\ApplicationService\DayOffFormDeleter;
+use App\DayOffForm\ApplicationService\DayOffFormRequestDeleter;
+use App\DayOffForm\ApplicationService\GetAllFormRequestByUser;
+use App\Entity\DayOffForm;
+use App\Entity\DayOffFormRequest;
 use App\User\ApplicationService\DTO\UserByIdRequest;
 use App\User\ApplicationService\Exception\UserNotFoundException;
 use App\User\Domain\UserDeleteRepository;
@@ -12,11 +17,17 @@ final class UserDeleter
 {
     private FindUserById $findUserById;
     private UserDeleteRepository $userDeleteRepository;
+    private GetAllFormRequestByUser $getAllFormRequestByUser;
+    private DayOffFormDeleter $dayOffFormDeleter;
+    private DayOffFormRequestDeleter $dayOffFormRequestDeleter;
 
-    public function __construct(FindUserById $findUserById, UserDeleteRepository $userDeleteRepository)
+    public function __construct(FindUserById $findUserById, UserDeleteRepository $userDeleteRepository, GetAllFormRequestByUser $getAllFormRequestByUser, DayOffFormDeleter $dayOffFormDeleter, DayOffFormRequestDeleter $dayOffFormRequestDeleter)
     {
         $this->findUserById = $findUserById;
         $this->userDeleteRepository = $userDeleteRepository;
+        $this->getAllFormRequestByUser = $getAllFormRequestByUser;
+        $this->dayOffFormDeleter =$dayOffFormDeleter;
+        $this->dayOffFormRequestDeleter = $dayOffFormRequestDeleter;
     }
 
     public function __invoke(UserByIdRequest $userByIdRequest)
@@ -27,6 +38,37 @@ final class UserDeleter
             throw new UserNotFoundException($userByIdRequest->userId());
         }
 
+        $dayOffFormAndDayOffFormRequestCollection = $this->mappingDayOffFormEntitiesAndDayOffFormRequest($this->getAllFormRequestByUser->__invoke($userEntity));
+        $dayOffFormCollection = $dayOffFormAndDayOffFormRequestCollection['dayOffForm'];
+        $dayOffFormRequestCollection = $dayOffFormAndDayOffFormRequestCollection['dayOffFormRequest'];
+
+        foreach ($dayOffFormRequestCollection as $dayOffFormRequestEntity) {
+            $this->dayOffFormRequestDeleter->__invoke($dayOffFormRequestEntity);
+        }
+
+        foreach ($dayOffFormCollection as $dayOffFormEntity) {
+            $this->dayOffFormDeleter->__invoke($dayOffFormEntity);
+        }
+
         $this->userDeleteRepository->deleteUser($userEntity);
+    }
+
+    public function mappingDayOffFormEntitiesAndDayOffFormRequest(array $dayOffFormRequestByUser): array
+    {
+        $dayOffFormEntitiesCollection = [];
+        $dayOffRequestEntitiesCollection = [];
+
+        foreach ($dayOffFormRequestByUser as $dayOff) {
+            if ($dayOff instanceof DayOffFormRequest) {
+                array_push($dayOffRequestEntitiesCollection, $dayOff);
+            }
+            if ($dayOff instanceof DayOffForm) {
+                array_push($dayOffFormEntitiesCollection, $dayOff);
+            }
+        }
+        return [
+            'dayOffForm' => $dayOffFormEntitiesCollection,
+            'dayOffFormRequest' => $dayOffRequestEntitiesCollection
+        ];
     }
 }
