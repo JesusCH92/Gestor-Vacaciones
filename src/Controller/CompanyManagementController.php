777c72d6a767d78ae8eac0972d9dfea4a355cf8d
miriam <ls28958@salleurl.edu>
2020-09-10 21:17:12 +0200

    add final to class

--- src/Controller/CompanyManagementController.php
+++ src/Controller/CompanyManagementController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CompanyManagementController extends AbstractController
+final class CompanyManagementController extends AbstractController
 {
     private GetAllDepartmentsByAdmin $getAllDepartmentsByAdmin;
     private CreateDepartment $createDepartment;
