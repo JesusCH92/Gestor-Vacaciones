ff3414c0fd7f0f87758d122ce84d7005fe39eac1
JesusCH92 <jcocaharo@hotmail.com>
2020-09-10 18:01:02 +0200

    update for adapter DepartmentUpdate port

--- src/Department/Infrastructure/OrmDepartmentRepository.php
+++ src/Department/Infrastructure/OrmDepartmentRepository.php
@@ -5,10 +5,11 @@ namespace App\Department\Infrastructure;
 
 
 use App\Department\Domain\DepartmentRepository;
+use App\Department\Domain\DepartmentUpdateRepository;
 use App\Entity\Department;
 use Doctrine\ORM\EntityManagerInterface;
 
-final class OrmDepartmentRepository implements DepartmentRepository
+final class OrmDepartmentRepository implements DepartmentRepository, DepartmentUpdateRepository
 {
     private EntityManagerInterface $entityManager;
 
@@ -21,6 +22,42 @@ final class OrmDepartmentRepository implements DepartmentRepository
     {
         $department = $this->entityManager->getRepository(Department::class);
         return $department->findAll();
+    }
+
+    public function findDepartmentById(string $departmentId): ?Department
+    {
+        $department = $this->entityManager->getRepository(Department::class);
+        return $department->find($departmentId);
+    }
+
+    public function departmentNameUpdate(string $departmentName, string $departmentId): void
+    {
+        $qb = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->update(Department::class, 'd')
+            ->set('d.departmentName',':departmentName')
+            ->where('d.departmentId = :departmentId')
+            ->setParameter('departmentName', $departmentName)
+            ->setParameter('departmentId', $departmentId)
+            ->getQuery()
+            ;
+        $qb->execute();
+    }
+
+    public function departmentCodeUpdate(string $departmentCode, string $departmentId): void
+    {
+        $qb = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->update(Department::class, 'd')
+            ->set('d.departmentCode',':departmentCode')
+            ->where('d.departmentId = :departmentId')
+            ->setParameter('departmentCode', $departmentCode)
+            ->setParameter('departmentId', $departmentId)
+            ->getQuery()
+            ;
+        $qb->execute();
     }
 
 }
