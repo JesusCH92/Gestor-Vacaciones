307c38d6bebf2d38eff627fb19f0d1f3e6f3e11e
miriam <ls28958@salleurl.edu>
2020-09-10 21:19:17 +0200

    add final to class

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegistrationController extends AbstractController
+final class RegistrationController extends AbstractController
 {
     /**
      * @Route("/user/management/register", name="app_register")
