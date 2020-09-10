950f5f141c8505381650804a1a90019e769fc590
miriam <ls28958@salleurl.edu>
2020-09-10 21:28:52 +0200

    add final to class

--- src/Controller/EditCompanyManagementController.php
+++ src/Controller/EditCompanyManagementController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EditCompanyManagementController extends AbstractController
+final class EditCompanyManagementController extends AbstractController
 {
     private UpdateCompany $updateCompany;
 
