7a323bc8b17236f55cc02a1f727e15eecbc1cb2a
miriam <ls28958@salleurl.edu>
2020-09-10 18:47:56 +0200

    add callbackError to the method of daysOffModel

--- public/assets/js/SuperviseEmployees/DaysOffEmployees/userInDayOffController.js
+++ public/assets/js/SuperviseEmployees/DaysOffEmployees/userInDayOffController.js
@@ -1,20 +1,26 @@
-var userInDayOffController = (function(){
+var userInDayOffController = (function () {
 
     var $userInDayOffFiltered = $(".user-in-dayOff-filtered");
     var $calendarContainer = $("#calendar-container");
 
     var _daysOffModel = daysOffModel();
     var _userInDayOffRenderTemplate = userInDayOffRenderTemplate();
+    var _errorModal = errorModal();
 
     var initEvent = function () {
-        $userInDayOffFiltered.click(function() {
+        $userInDayOffFiltered.click(function () {
             var $userId = $(this).attr('userId');
             console.log($userId);
-            _daysOffModel.getCalendarWithUserInDayOffByCurrentYear({id : $userId, callback : _userInDayOffRenderTemplate.paintCalendarForUserInDayOffByCurrentYear, container: $calendarContainer});
+            _daysOffModel.getCalendarWithUserInDayOffByCurrentYear({
+                id: $userId,
+                callback: _userInDayOffRenderTemplate.paintCalendarForUserInDayOffByCurrentYear,
+                container: $calendarContainer,
+                callbackError: _errorModal.paintErrorModal
+            });
         });
     };
 
     return {
-        initEventUserInDayOff : initEvent
+        initEventUserInDayOff: initEvent
     }
 });
