86f1f66f6fc98b611f496ba8dedff2334a725003
JesusCH92 <jcocaharo@hotmail.com>
2020-09-10 17:53:36 +0200

    create exception if dept not exists

---
+++ src/Department/ApplicationService/Exception/DepartmentNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Department\ApplicationService\Exception;
+
+use Exception;
+use Throwable;
+
+final class DepartmentNotFoundException extends Exception
+{
+    public function __construct(string $departmentId, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct(
+            sprintf('Department with id "%s" were not found', $departmentId),
+            $code,
+            $previous
+        );
+    }
+}
