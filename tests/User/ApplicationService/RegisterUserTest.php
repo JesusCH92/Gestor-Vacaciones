404d32bd5f34fd487bccc968b6f38aec5441193c
miriam <ls28958@salleurl.edu>
2020-09-12 12:19:26 +0200

    create test class for test the register of an user

---
+++ tests/User/ApplicationService/RegisterUserTest.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Tests\Calendar\User\ApplicationService;
+
+
+use App\Entity\Company;
+use App\Entity\Department;
+use App\Tests\User\Infrastructure\OrmUserFactoryDummy;
+use App\Tests\User\Infrastructure\OrmUserRepositoryInvalidStub;
+use App\Tests\User\Infrastructure\OrmUserRepositoryStub;
+use App\User\ApplicationService\DTO\RegisterUserRequest;
+use App\User\ApplicationService\Exception\AlreadyExistingUserException;
+use App\User\ApplicationService\RegisterUser;
+use App\User\Domain\Exception\InvalidRoleException;
+use PHPUnit\Framework\TestCase;
+
+class RegisterUserTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function userAlreadyExists()
+    {
+        $this->expectException(AlreadyExistingUserException::class);
+
+        $company = new Company();
+        $registerUserRequest = new RegisterUserRequest('miriam','lopez','663786798','miriam@gmail.com','password',new Department('TFM','t', $company),$company,'ROLE_USER');
+
+        $ormUserFactoryDummy = new OrmUserFactoryDummy();
+        $ormUserRepositoryStub = new OrmUserRepositoryInvalidStub();
+
+        $registerUser = new RegisterUser($ormUserFactoryDummy,$ormUserRepositoryStub);
+        $registerUser->__invoke($registerUserRequest);
+
+    }
+
+    /**
+     * @test
+     */
+    public function notValidRole()
+    {
+        $this->expectException(InvalidRoleException::class);
+        $company = new Company();
+        $registerUserRequest = new RegisterUserRequest('miriam','lopez','663786798','miriam@gmail.com','password',new Department('TFM','t', $company),$company,'ROLE_EMPLOYEE');
+
+        $ormUserFactoryDummy = new OrmUserFactoryDummy();
+        $ormUserRepositoryStub = new OrmUserRepositoryStub();
+
+        $registerUser = new RegisterUser($ormUserFactoryDummy,$ormUserRepositoryStub);
+        $registerUser->__invoke($registerUserRequest);
+    }
+}
