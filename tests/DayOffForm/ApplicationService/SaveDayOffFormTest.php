38b2f5424f19c81dc43e60ce2a357bc56987d497
miriam <ls28958@salleurl.edu>
2020-09-13 21:15:07 +0200

    add method to check the correct behavior of save day off form

--- tests/DayOffForm/ApplicationService/SaveDayOffFormTest.php
+++ tests/DayOffForm/ApplicationService/SaveDayOffFormTest.php
@@ -16,9 +16,11 @@ use App\Entity\Calendar;
 use App\Entity\Company;
 use App\Entity\Department;
 use App\Tests\DayOffForm\Infrastructure\OrmDayOffRepositoryDummy;
+use App\Tests\DayOffForm\Infrastructure\OrmDayOffRepositorySpy;
 use App\User\Domain\ValueObject\Roles;
 use App\User\Infrastructure\Model\SymfonyUser;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Constraints\Date;
 
 class SaveDayOffFormTest extends TestCase
 {
@@ -107,5 +109,28 @@ class SaveDayOffFormTest extends TestCase
         $saveDayOffRequest = new SaveDayOffRequest($ormDayOffRepositoryDummy);
 
         $saveDayOffRequest->__invoke($dayOffRequest);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSaveDayOffForm()
+    {
+        $company = new Company();
+        $user = new SymfonyUser('miriam@gmail.com', 'miriam', 'lopez', '663768798', new Roles('ROLE_SUPERVISOR'),
+            'miriam', new Department('TFM', 't', $company), $company);
+
+        $calendar = new Calendar(new DayOffConfig('2020-01-01', '2021-01-31'), new WorkDays(["1", "2", "3"]),
+            new Company(), new WorkingYear(2020));
+
+        $date = new \DateTime('now');
+        $dayOffRequest = new DayOffRequest($user, $calendar, 'Holiday', [$date->format('Y-m-d')],
+            ['Holiday' => 3, 'Personal' => 1], [], 2);
+
+        $ormDayOffRepositorySpy = new OrmDayOffRepositorySpy();
+        $saveDayOffRequest = new SaveDayOffRequest($ormDayOffRepositorySpy);
+        $saveDayOffRequest->__invoke($dayOffRequest);
+
+        $this->assertTrue($ormDayOffRepositorySpy->verify());
     }
 }
