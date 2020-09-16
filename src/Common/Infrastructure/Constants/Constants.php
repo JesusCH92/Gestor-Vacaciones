011c4fcf6d497bb19fc60854681fe14a3326904a
JesusCH92 <jcocaharo@hotmail.com>
2020-09-16 13:21:53 +0200

    create constant file, now file contents user id for admin

---
+++ src/Common/Infrastructure/Constants/Constants.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Common\Infrastructure\Constants;
+
+final class Constants
+{
+    public const ADMIN_ID = '2609380a-bf2c-4db3-9e6f-1e622e0e6845';
+}
