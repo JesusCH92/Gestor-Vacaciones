7ac8ca00373069689aa8083a28b9444cf53952c0
miriam <ls28958@salleurl.edu>
2020-09-16 20:20:57 +0200

    refactor rename style class and ids to be explicits and in english

--- public/assets/js/ErrorModal/errorModalTemplate.js
+++ public/assets/js/ErrorModal/errorModalTemplate.js
@@ -2,7 +2,7 @@ var errorModal = (function (){
 
     var paintErrorModal = function ({message_error}) {
         // $("#main-container").append(_modal);
-        $("#modal-error-texto").text(message_error);
+        $("#modal-error-text").text(message_error);
         $("#modal-error").modal('show');
         // return _modal;
     };
