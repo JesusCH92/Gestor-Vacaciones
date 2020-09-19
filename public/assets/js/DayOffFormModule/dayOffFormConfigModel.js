2c651ededbc1defa06f15f6f5ca0fb5434b3a9e7
JesusCH92 <jcocaharo@hotmail.com>
2020-09-19 23:30:34 +0200

    refactor saveDayOffFormRequest for show modal(if request is valid)  and reset form

--- public/assets/js/DayOffFormModule/dayOffFormConfigModel.js
+++ public/assets/js/DayOffFormModule/dayOffFormConfigModel.js
@@ -1,14 +1,20 @@
 var dayOffFormConfigModel = (function() {
 
-    var saveDayOffFormRequest = function ({day_off_request, callback = console.log, callbackError = console.log}) {
+    var saveDayOffFormRequest = function ({day_off_request, callback = console.log, callbackError = console.log, successModal = console.log}) {
         $.ajax({
             type: 'POST',
             url: '/dayoff/request/management/add',
             async: true,
             data: {day_off_request},
             success: function(data){
-                callback(data);
-                location.reload();
+                successModal({
+                    message : `${ JSON.parse(day_off_request['days_off']).length } days were request successfully`,
+                    message_title : 'Day Off Request'
+                });
+                callback({
+                    dayoff_type : day_off_request['type_of_day'].toLowerCase(), 
+                    dayoff_count : JSON.parse(day_off_request['days_off']).length
+                });
             },
             error: function(data){
                 callbackError({message_error: data.responseText});
