31c59aecb25027424ed4ab7fc468d675c55d8410
JesusCH92 <jcocaharo@hotmail.com>
2020-09-19 05:43:15 +0200

    add validation for all field in create calendar view

--- public/assets/js/CalendarModule/calendarController.js
+++ public/assets/js/CalendarModule/calendarController.js
@@ -8,7 +8,13 @@ var calendarController = (function(){
     var $workDaysSelected = $("#work-days-select");
     var $feastdayDateInput = $("#feastday-date");
     var $feastdaySelectedContainer = $("#feastday-selected-container");
+    var $initDateErrorSpan = $("#init-date-request-error");
+    var $endDateErrorSpan = $("#end-date-request-error");
+    var $holidayErrorSpan = $("#holidays-number-error");
+    var $personalDayErrorSpan = $("#personal-days-number-error");
+    var $feastDayErrorSpan = $("#feastday-date-error");
 
+    var _validator = validator();
     var _calendarCreatorModel = calendarCreatorModel();
     var _calendarCreatorRenderTemplate = calendarCreatorRenderTemplate();
     var _errorModal = errorModal();
@@ -25,7 +31,14 @@ var calendarController = (function(){
     var initEvents = function() {
         $feastdayDateInput.change(function(){
             var $feastdaySelected = $feastdayDateInput.val();
-            console.log($feastdaySelected);
+
+            if ( !_validator.isValidDate($feastdaySelected) ) {
+                _calendarCreatorRenderTemplate.paintErrorMessage({
+                    errorId : $feastDayErrorSpan,
+                    errorMessage : 'Date is not valid. Must be 01-01-2019.'
+                });
+                return;
+            }
 
             _calendarCreatorRenderTemplate.paintFeastDayInFeastDayContainer({
                 feastday : $feastdaySelected,
@@ -39,7 +52,6 @@ var calendarController = (function(){
             }
             
             var $feastdayRowToDelete = $(event.target).parent().parent();
-            console.log($feastdayRowToDelete);
             $feastdayRowToDelete.remove();
         });
 
@@ -52,6 +64,50 @@ var calendarController = (function(){
             var $workDays = $workDaysSelected.val();
             var $feastDayCollection = getAllFeastdaySelectedInContainer( { feastdaySelected : "#feastday-selected-container p" } );
 
+            if ( !_validator.isValidDate($initDateRequest) ) {
+                _calendarCreatorRenderTemplate.paintErrorMessage({
+                    errorId : $initDateErrorSpan,
+                    errorMessage : 'Init date is not valid. Must be greater than 01-01-2019.'
+                });
+                return;
+            }
+
+            if ( !_validator.isValidDate($endDateRequest) ) {
+                _calendarCreatorRenderTemplate.paintErrorMessage({
+                    errorId : $endDateErrorSpan,
+                    errorMessage : 'End date is not valid. Must be greater than 01-01-2019.'
+                });
+                return;
+            }
+
+            if ( !_validator.endDateIsGreaterThanInitDate($initDateRequest, $endDateRequest) ) {
+                _calendarCreatorRenderTemplate.paintErrorMessage({
+                    errorId : $initDateErrorSpan,
+                    errorMessage : 'End date must be greater than init date.'
+                });
+                _calendarCreatorRenderTemplate.paintErrorMessage({
+                    errorId : $endDateErrorSpan,
+                    errorMessage : 'End date must be greater than init date.'
+                });
+                return;
+            }
+
+            if ( _validator.isIntegerPositive($holidaysNumber) ) {
+                _calendarCreatorRenderTemplate.paintErrorMessage({
+                    errorId : $holidayErrorSpan,
+                    errorMessage : 'A type day off count could not be negative.'
+                });
+                return;
+            }
+
+            if ( _validator.isIntegerPositive($personalDaysNumber) ) {
+                _calendarCreatorRenderTemplate.paintErrorMessage({
+                    errorId : $personalDayErrorSpan,
+                    errorMessage : 'A type day off count could not be negative.'
+                });
+                return;
+            }
+
             _calendarCreatorModel.createCalendar({ 
                 calendar : {
                     workingYear : $workingYear,
@@ -62,6 +118,7 @@ var calendarController = (function(){
                     workDays : $workDays.length === 0 ? '' : $workDays,
                     feastDayCollection : $feastDayCollection.length === 0 ? '' : $feastDayCollection
                 },
+                callback : _calendarCreatorRenderTemplate.cleanCalendarForm,
                 callbackError : _errorModal.paintErrorModal
             });
         });
