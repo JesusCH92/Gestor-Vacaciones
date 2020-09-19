0ee2207dc0a6142b95753b98e56b7a41b0a0adc3
JesusCH92 <jcocaharo@hotmail.com>
2020-09-19 08:31:25 +0200

    update modal for success or failed with style

--- public/assets/js/ErrorModal/errorModalTemplate.js
+++ public/assets/js/ErrorModal/errorModalTemplate.js
@@ -1,12 +1,36 @@
 var errorModal = (function (){
 
     var paintErrorModal = function ({ message_error, message_title = 'ERROR' }) {
+        if ( $("#modal-content").hasClass("modal--success-container") ) {
+            $("#modal-content").removeClass("modal--success-container");
+        }
+
+        if ( $("#modal-button").hasClass("btn-success") ) {
+            $("#modal-button").removeClass("btn-success");
+        }
+
+        if ( !$("#modal-button").hasClass("btn-danger") ) {
+            $("#modal-button").addClass("btn-danger");
+        }
+
         $(".modal-title").text(message_title);
         $("#modal-error-text").text(message_error);
         $("#modal-error").modal('show');
     };
 
     var paintSuccessModal = function ({ message, message_title }) {
+        if ( !$("#modal-content").hasClass("modal--success-container") ) {
+            $("#modal-content").addClass("modal--success-container");
+        }
+
+        if ( $("#modal-button").hasClass("btn-danger") ) {
+            $("#modal-button").removeClass("btn-danger");
+        }
+
+        if ( !$("#modal-button").hasClass("btn-success") ) {
+            $("#modal-button").addClass("btn-success");
+        }
+
         $(".modal-title").text(message_title);
         $("#modal-error-text").text(message);
         $("#modal-error").modal('show');
