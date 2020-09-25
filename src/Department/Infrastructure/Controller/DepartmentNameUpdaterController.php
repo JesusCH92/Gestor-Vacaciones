d73570ee8d5c040189cd2e057de66818fec0a18d
JesusCH92 <jcocaharo@hotmail.com>
2020-09-25 23:03:36 +0200

    refactor-> move controllers to Department Module, structure who are and what type

--- src/Department/Infrastructure/Controller/DepartmentNameUpdaterController.php
+++ src/Department/Infrastructure/Controller/DepartmentNameUpdaterController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace App\Controller;
+namespace App\Department\Infrastructure\Controller;
 
 use App\Department\ApplicationService\DepartmentNameUpdater;
 use App\Department\ApplicationService\DTO\DepartmentNameRequest;
